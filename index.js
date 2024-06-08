38b681510d11044a5db660b897ceff7f565caf57
TUTULROY <tutulroyjoy@gmail.com>
2024-06-09 02:30:51 +0600

    Added carts collection

--- index.js
+++ index.js
@@ -57,6 +57,14 @@ async function run() {
         res.send(result);
     }),
 
+
+    app.get('/carts', async (req, res) =>{
+        const email = req.query.email;
+        const query = { email: email};
+        const result = await cartCollection.find(query).toArray();
+        res.send(result);
+    })
+
     app.post('/carts', async(req, res)=>{
         const cart = req.body;
         const result = await cartCollection.insertOne(cart);
